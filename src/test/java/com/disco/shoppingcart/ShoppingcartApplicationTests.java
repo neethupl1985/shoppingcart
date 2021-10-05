624810a86dc83c75bc8ff44659b3cdfb9116dfd3
neethupl1985 <neetit.it@gmail.com>
2021-10-05 11:20:59 -0400

    Skeleton project
    Skeleton project

---
+++ src/test/java/com/disco/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disco.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
